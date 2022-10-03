ee4516952d2c15e8a8db57922f70c9fb0676fb9c
Marcos Pereira <mvnpereira@gmail.com>
2022-10-03 09:57:05 -0300

    Enviando banco de dados atualizado

--- copa/app/Models/ConfrontoPalpite.php
+++ copa/app/Models/ConfrontoPalpite.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ConfrontoPalpite extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
+
+    protected $table = 'confronto_palpites';
+
+    protected $fillable = [
+
+    ];
 }
